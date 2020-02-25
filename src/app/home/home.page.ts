4b86fe95cf6899c72a1009a01270f0b0c6156954
Himanshu <himanshupednekar036@gmail.com>
2020-02-25 18:09:10 +0530

    profile page done

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { ActionSheetController } from "@ionic/angular";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"]
 })
 export class HomePage {
+  constructor(public actionSheetController: ActionSheetController) {}
+  async presentActionSheet() {
+    const actionSheet = await this.actionSheetController.create({
+      header: "Albums",
+      buttons: [
+        {
+          text: "Delete",
+          role: "destructive",
+          icon: "trash",
 
-  constructor() {}
+          handler: () => {
+            console.log("Delete clicked");
+          }
+        },
+        {
+          text: "Share",
+          icon: "share",
+          handler: () => {
+            console.log("Share clicked");
+          }
+        },
 
+        {
+          text: "Cancel",
+          icon: "close",
+          role: "cancel",
+          handler: () => {
+            console.log("Cancel clicked");
+          }
+        }
+      ]
+    });
+    await actionSheet.present();
+  }
 }
