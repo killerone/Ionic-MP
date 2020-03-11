dc02164f9ebd023963e601f339248e70a3adc5b2
Himanshu <himanshupednekar036@gmail.com>
2020-03-11 11:01:34 +0530

    get done

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
 export class HomePage {
+  link = 'https://reqres.in/api/users?page=0&per_page=12';
+  users = [];
 
-  constructor() {}
+  constructor(private http: HttpClient) {
+    http.get(this.link).subscribe(data => {
+      this.users = data.data;
+    });
+  }
 
+  deleteUser(id) {
+    this.users.splice(id, 1);
+  }
 }
