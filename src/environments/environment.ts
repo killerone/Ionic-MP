f4b4583c8727f01c7902ae43d82f77d10c43aa2d
Himanshu <himanshupednekar036@gmail.com>
2020-03-11 10:15:29 +0530

    firebase added but not tested

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,9 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firsbaseConfig: {
+    apiKey: 'AIzaSyDNuh4dPY99Uxqq4VWCnm6jDDO9kiliDG0',
+    authDomain: 'too-easy-order.firebaseapp.com',
+    databaseURL: 'https://too-easy-order.firebaseio.com',
+    projectId: 'too-easy-order',
+    storageBucket: 'too-easy-order.appspot.com',
+    messagingSenderId: '417271147874',
+    appId: '1:417271147874:web:60c77bafe4a513ab'
+  }
 };
-
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
