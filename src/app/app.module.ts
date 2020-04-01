542bab25fae46b920c891fd304a5f6fa1e62a5fe
Himanshu <himanshupednekar036@gmail.com>
2020-04-01 09:07:10 +0530

    working code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
-import { File } from '@ionic-native/File/ngx';
+import { File } from '@ionic-native/file/ngx';
 import { FileOpener } from '@ionic-native/file-opener/ngx';
 import { FileTransfer } from '@ionic-native/file-transfer/ngx';
 import { DocumentViewer } from '@ionic-native/document-viewer/ngx';
@@ -29,4 +29,4 @@ import { DocumentViewer } from '@ionic-native/document-viewer/ngx';
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,54 +1,90 @@
-import { Platform } from '@ionic/angular';
-import { DocumentViewer, DocumentViewerOptions } from '@ionic-native/document-viewer/ngx';
-import { FileTransfer } from '@ionic-native/file-transfer/ngx';
-import { FileOpener } from '@ionic-native/file-opener/ngx';
-import { File } from '@ionic-native/File/ngx';
-import { Component } from '@angular/core';
-
+import { Component } from "@angular/core";
+import { Platform, AlertController } from "@ionic/angular";
+import {
+  DocumentViewer,
+  DocumentViewerOptions
+} from "@ionic-native/document-viewer/ngx";
+import { File } from "@ionic-native/file/ngx";
+import {
+  FileTransfer,
+  FileTransferObject
+} from "@ionic-native/file-transfer/ngx";
+import { HttpClientModule } from "@angular/common/http";
+//import { HttpModule } from "@angular/http";
+declare var require: any;
+const FileSaver = require("file-saver");
+import { FileOpener } from "@ionic-native/file-opener/ngx";
+//import { FileChooser } from "@ionic-native/file-chooser/ngx";
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"]
 })
 export class HomePage {
-
+  ft: any;
+  // fileOpener: any;
+  fileTransfer: FileTransferObject;
   constructor(
     private platform: Platform,
     private file: File,
-    private fileOpener: FileOpener,
-    private ft: FileTransfer,
-    private document: DocumentViewer) { }
-
-  openLocalPdf() {
-    const filePath = this.file.applicationDirectory + 'www/assets';
-
-    if (this.platform.is("android")) {
-      const newName = Date.now();
-      this.file.copyFile(filePath, 'MDB.pdf', this.file.dataDirectory, `${newName}.pdf`).then(res => {
-        this.fileOpener.open(res.nativeURL, 'application/pdf');
-      });
-
-    } else {
-      const options: DocumentViewerOptions = {
-        title: "Himan PDF"
-      };
-      this.document.viewDocument(`${filePath}/MDB.pdf`, 'application/pdf', options);
-    }
+    private transfer: FileTransfer,
+    private document: DocumentViewer,
+    private alertController: AlertController,
+    private fileOpener: FileOpener //private fileChooser: FileChooser
+  ) {
+    this.fileTransfer = this.transfer.create();
   }
 
+  openLocalPdf() {
+    const opt: DocumentViewerOptions = {
+      title: "My PDF"
+    };
+    FileSaver.saveAs(
+      "https://devdactic.com/html/5-simple-hacks-LBT.pdf",
+      "myFile.pdf"
+    );
+  }
 
   downloadAndOpenLocalPdf() {
-    const downloadURL = "https://devdactic.com/html/5-simple-hacks-LBT.pdf"
-    const path = this.file.dataDirectory;
-    const transfer = this.ft.create();
+    /*  let path = null;
+    path = this.file.dataDirectory;
+    // this.presentAlert("CHECK" + this.file);
 
-    transfer.download(downloadURL, `${path}myfile.pdf`).then(entry => {
-      const url = entry.toURL();
-      if (this.platform.is("ios")) {
-        this.document.viewDocument(url, 'application/pdf', {});
-      } else {
-        this.fileOpener.open(url, 'application/pdf');
-      }
+    //this.presentAlert("CHECK" + this.fileTransfer);
+    //let proxyurl = "https://cors-anywhere.herokuapp.com/";
+    this.fileTransfer
+      .download(
+        "https://www.adobe.com/support/products/enterprise/knowledgecenter/media/c4611_sample_explain.pdf",
+        path + "myFile.pdf"
+      )
+      .then(entry => {
+        console.log("download complete: " + entry.toURL());
+        this.fileOpener
+          .open(entry.toURL(), "application/pdf")
+          .then(() => console.log("File is opened"))
+          .catch(e => console.log("Error opening file", e));
+      });*/
+    let url = "https://devdactic.com/html/5-simple-hacks-LBT.pdf";
+    FileSaver.saveAs(
+      "https://cors-anywhere.herokuapp.com/" + url,
+      "myFile.pdf"
+    );
+
+    FileSaver.saveAs(url, "myFile.pdf");
+  }
+  async presentAlert(error) {
+    const alert = await this.alertController.create({
+      header: " ",
+      message: error,
+      buttons: [
+        {
+          text: "Cancel",
+          role: "cancel",
+          cssClass: "secondary"
+        }
+      ]
     });
+
+    await alert.present();
   }
 }
